cef96f2eb21b4a7fb0387630c452df82009d7773
mickael <nv.osselaer@gmail.com>
2021-01-17 23:40:10 +0100

    memoryV2

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let choiceBody = document.querySelector('#theme');
 let choiceBackGroundLeft = document.getElementById('container_left');
 let backgroundCard = document.getElementById('card');
-let centuryFox = document.getElementById('centuryFox');
 let buttonStart = document.getElementById('start');
 let choiceLevel = document.getElementById('level');
 let label = document.getElementsByTagName('label');
 let border = document.querySelector('.border');
 let tittleScore = document.querySelector('.tittle');
+let backCard = document.querySelectorAll('.back_card');
+let myImagies = document.getElementsByTagName('img');
+let tittleGame = document.getElementsByClassName('tittle_game');
+let nemo = document.getElementsByClassName('nemoimg');
+let cinema = document.getElementsByClassName('cinemaimg');
+let divCardNemo = document.getElementById('nemo');
 
 choiceBody.addEventListener('change', (e) => {
     if (choiceBody.value === 'nemo') {
@@ -15,13 +20,43 @@ choiceBody.addEventListener('change', (e) => {
         choiceBackGroundLeft.style.backgroundSize = "cover";
         backgroundCard.style.backgroundImage = "url('le-monde-de-nemo.jpg')";
         backgroundCard.style.border = "2px solid white";
-        centuryFox.style.display = "none";
+        divCardNemo.style.display ="flex";
+
+
+        for(let presentationCinema of cinema) {
+            presentationCinema.style.display = "none";
+        }
+
+        for(let presentationNemo of nemo) {
+            presentationNemo.style.border = "2px solid white";
+            presentationNemo.style.display = "flex";
+            presentationNemo.style.flexWrap = "wrap";
+            presentationNemo.style.justifyContent = "center";
+            presentationNemo.style.alignItems = "center";
+        }
+
+        for(let tittle of tittleGame) {
+            tittle.style.color = "#a6c0fe";
+        }
+
     }
     else if (choiceBody.value === 'cinema') {
         document.body.style.backgroundImage = "url('image_background.png')";
-        choiceBackGroundLeft.style.backgroundImage = "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)";
-        backgroundCard.style.backgroundImage = "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)";
-        centuryFox.style.display = "flex"
+        choiceBackGroundLeft.style.backgroundImage = "url('image_card_20thCF.jpg')";
+        backgroundCard.style.backgroundImage = "url('image_card_20thCF.jpg')";
+        backgroundCard.style.border = "2px solid gold";
+
+        for(let presentationCinema of cinema) {
+            presentationCinema.style.display = "flex";
+        }
+
+        for(let presentationNemo of nemo) {
+            presentationNemo.style.display = "none";
+        }
+
+        for(let tittle of tittleGame) {
+           tittle.style.color = "gold";
+        }
     }
 })
 
@@ -30,14 +65,48 @@ buttonStart.addEventListener('click', (e) => {
         choiceLevel.style.display = "none";
         buttonStart.style.display = "none";
         backgroundCard.style.display = "none";
-        centuryFox.style.display = "none";
+
+        for(let tittle of tittleGame) {
+            tittle.style.display = "none";
+        }
+
+        for(let image of myImagies) {
+            image.style.display = "none";
+        }
+
         for(let lab of label){
             lab.innerHTML = "";
         }
+
         border.style.display = "none";
         tittleScore.style.margin = "2rem";
+
+
+        for(let card of backCard) {
+
+            if(choiceBody.value === 'cinema'){
+                card.style.display = "flex";
+                card.style.border = "2px solid gold";
+                card.style.borderRadius = "1.5rem";
+                card.style.width = "16%";
+                card.style.height = "15vh";
+                card.style.margin = "2rem";
+                card.style.backgroundSize = "cover";
+                card.style.backgroundImage = "url('image_card_20thCF.jpg')";
+
+            }
+
+            else if (choiceBody.value === 'nemo') {
+                card.style.display = "flex";
+                card.style.border = "2px solid gold";
+                card.style.borderRadius = "1.5rem";
+                card.style.width = "16%";
+                card.style.height = "15vh";
+                card.style.margin = "2rem";
+                card.style.backgroundImage = "url('le-monde-de-nemo.jpg')";
+                card.style.backgroundSize = "cover";
+            }
+        }
 })
-
-
 
 
